51241e6e08486fd03ed0a84951ddaec59b18b88b
Rohit Debnath <rohit.kumardebnath.cse28@heritageit.edu.in>
2026-03-22 12:36:38 +0530

    2

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 8080; // Flux usually targets 8080
 app.get('/', (req, res) => {
   res.json({
     status: 'success',
-    message: 'Hello World! This app was successfully deployed to the decentralized Web3 Flux Network via AlgoFlow!',
+    message: 'Hogya hogyaaa hogyaaa!',
     timestamp: new Date().toISOString()
   });
 });
